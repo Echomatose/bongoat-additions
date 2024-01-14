1b0236b0db91aaef3d65198b353b92989b5df907
Echomatose <alexanderberno@gmail.com>
2024-01-14 11:25:56 -0500

    added right click capability (will not work properly until 3d model implemented)

---
+++ bongoat-1.20.1/src/main/java/net/echomatose/bongoatmod/item/BongoatItem.java
@@ -0,0 +1,41 @@
+package net.echomatose.bongoatmod.item;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.common.ToolActions;
+
+public class BongoatItem extends Item {
+
+    public BongoatItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pUsedHand);
+        pPlayer.startUsingItem(pUsedHand);
+        return InteractionResultHolder.consume(itemstack);
+    }
+
+    @Override
+    public boolean canPerformAction(ItemStack stack, ToolAction toolAction) {
+        return ToolActions.DEFAULT_SHIELD_ACTIONS.contains(toolAction);
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack pStack) {
+        return UseAnim.BLOCK;
+    }
+
+    @Override
+    public int getUseDuration(ItemStack pStack) {
+        return 72000;
+    }
+
+}
--- bongoat-1.20.1/src/main/java/net/echomatose/bongoatmod/item/ModItems.java
+++ bongoat-1.20.1/src/main/java/net/echomatose/bongoatmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.echomatose.bongoatmod.item;
 
 import net.echomatose.bongoatmod.BongoatMod;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ShieldItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -10,10 +11,11 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BongoatMod.MODID);
 
-    public static final RegistryObject<Item> BONGOAT = ITEMS.register("bongoat", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BONGOAT = ITEMS.register("bongoat",
+            () -> new BongoatItem(new Item.Properties()));
 
     public static final RegistryObject<Item> BONGOAT_SMILING = ITEMS.register("bongoat_smiling",
-            () -> new Item(new Item.Properties()));
+            () -> new BongoatItem(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
