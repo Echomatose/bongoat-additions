7d11789c68fc1df3cf2ac823f1092483bd37e117
Echomatose <alexanderberno@gmail.com>
2024-01-14 10:40:16 -0500

    made bongoat block drop on break

--- bongoat-1.20.1/src/main/java/net/echomatose/bongoatmod/block/ModBlocks.java
+++ bongoat-1.20.1/src/main/java/net/echomatose/bongoatmod/block/ModBlocks.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -19,7 +20,7 @@ public class ModBlocks {
             BongoatMod.MODID);
 
     public static final RegistryObject<Block> BONGOAT_BLOCK = registerBlock("bongoat_block",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.WHITE_WOOL)));
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.WOOL)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
